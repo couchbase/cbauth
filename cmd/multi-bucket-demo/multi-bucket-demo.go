abda7dc7283d16ef487bedc6735d214fff674764
Aliaksey Kandratsenka <alk@tut.by>
2014-12-19 17:04:40 -0800

    added multi-bucket-demo program
    Change-Id: I91a70105069824bf3575b726bfaa4ade21f7774a
    Reviewed-on: http://review.couchbase.org/44549
    Reviewed-by: Manik Taneja <manik@couchbase.com>
    Tested-by: Aliaksey Kandratsenka <alkondratenko@gmail.com>
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>

---
+++ cmd/multi-bucket-demo/multi-bucket-demo.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/couchbase/cbauth"
+	couchbase "github.com/couchbaselabs/go-couchbase"
+)
+
+var serverURL = flag.String("serverURL", "http://localhost:9000", "couchbase server URL")
+var poolName = flag.String("poolName", "default", "pool name")
+var bucketName = flag.String("bucketName", "default", "bucket name")
+
+var keyToSet = flag.String("keyToSet", "foo", "key to Set")
+var valueToSet = flag.String("valueToSet", "bar", "value to Set")
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", os.Args[0])
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "\nNOTE: that this program requires correctly set cbauth env variables.")
+		fmt.Fprintf(os.Stderr, "E.g: NS_SERVER_CBAUTH_URL=http://127.0.0.1:9000/_cbauth \\\n"+
+			" NS_SERVER_CBAUTH_USER=Administrator NS_SERVER_CBAUTH_PWD=asdasd \\\n"+
+			"%s --serverURL=http://10.17.3.172:9000 --bucketName=bucket-foo\n\n", os.Args[0])
+	}
+	flag.Parse()
+
+	// this will give go-couchbase unlimited access to all http
+	// endpoints it has access to.
+	//
+	// NOTE: it's kinda hackish. And note that it _will override_
+	// whatever auth is given at client instantiation. In this
+	// program there is _no auth_ that is given anywhere as auth
+	// is supposed to come from cbauth.
+	transport := cbauth.WrapHTTPTransport(couchbase.HTTPTransport, nil)
+	couchbase.HTTPClient.Transport = transport
+
+	client, err := couchbase.ConnectWithAuth(*serverURL, cbauth.NewAuthHandler(nil))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	p, err := client.GetPool("default")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := p.GetBucket(*bucketName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Setting key `%s' to `%s' in bucket `%s'", *keyToSet, *valueToSet, *bucketName)
+	err = b.SetRaw(*keyToSet, 0, []byte(*valueToSet))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
