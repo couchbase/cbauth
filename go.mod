1aacc33af7a3a5715dc7882b0f4dfb1ad974eccb
Neelima Premsankar <neelima.premsankar@couchbase.com>
2023-03-24 10:03:22 -0700

    MB-55799: Remove ExtractCreds/ExtractOnBehalfIdentity.
    cbgt/query no longer use these, deprecate them.
    Update go.mod.
    
    Change-Id: If67a21530f7acaf1984f8776d1308fa6ea61fbb7
    Reviewed-on: https://review.couchbase.org/c/cbauth/+/188751
    Well-Formed: Build Bot <build@couchbase.com>
    Tested-by: Neelima Premsankar <neelima.premsankar@couchbase.com>
    Reviewed-by: Hareen Kancharla <hareen.kancharla@couchbase.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module github.com/couchbase/cbauth
 
-go 1.13
+go 1.19
+
+require (
+	github.com/couchbase/clog v0.1.0 // indirect
+	github.com/couchbase/go-couchbase v0.1.1 // indirect
+	github.com/couchbase/gomemcached v0.2.1 // indirect
+	github.com/couchbase/goutils v0.1.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+)
--- utils.go
+++ utils.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"net"
-	"net/http"
 	"strconv"
 	"strings"
 
@@ -42,6 +41,7 @@ func extractBase64Pair(s string) (user, extra string, err error) {
 	return
 }
 
+// ExtractCredsGeneric extracts Basic auth creds from header.
 func ExtractCredsGeneric(hdr httpreq.HttpHeader) (user string, pwd string, err error) {
 	auth := hdr.Get("Authorization")
 	if auth == "" {
@@ -54,17 +54,6 @@ func ExtractCredsGeneric(hdr httpreq.HttpHeader) (user string, pwd string, err e
 		return
 	}
 	return extractBase64Pair(auth[len(basicPrefix):])
-}
-
-// TODO: Remove this when query moves to using httpreq alone
-func ExtractCreds(req *http.Request) (user string, pwd string, err error) {
-	return ExtractCredsGeneric(req.Header)
-}
-
-// TODO: Remove this when query moves to using httpreq alone
-func ExtractOnBehalfIdentity(req *http.Request) (user string,
-	domain string, err error) {
-	return ExtractOnBehalfIdentityGeneric(req.Header)
 }
 
 // ExtractOnBehalfIdentityGeneric extracts 'on behalf' identity from header.
