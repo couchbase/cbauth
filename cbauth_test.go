ba24430e4d2d0471df74b712215b365d6a809799
Artem Stemkovski <artem@couchbase.com>
2023-06-01 19:18:26 -0700

    MB-56894 basic auth automated test
    Change-Id: I2ac62430fc62ec7193f7e5a12927acdff51860c6
    Reviewed-on: https://review.couchbase.org/c/cbauth/+/191948
    Well-Formed: Restriction Checker
    Tested-by: Artem Stemkovski <artem@couchbase.com>
    Reviewed-by: Timofey Barmin <timofey.barmin@couchbase.com>

--- cbauth_test.go
+++ cbauth_test.go
@@ -47,17 +47,6 @@ func acc(ok bool, err error) bool {
 	return ok
 }
 
-func assertAdmins(t *testing.T, c Creds, needAdmin, needROAdmin bool) {
-	if acc(c.IsAllowed("cluster.admin.settings!write")) != needAdmin {
-		t.Fatalf("admin access must be: %v", needAdmin)
-	}
-	roadmin := !acc(c.IsAllowed("cluster.admin.settings!write")) &&
-		acc(c.IsAllowed("cluster.admin.security!read"))
-	if roadmin != needROAdmin {
-		t.Fatalf("ro-admin access must be: %v", needROAdmin)
-	}
-}
-
 func newCache(a *authImpl) *cbauthimpl.Cache {
 	url := "http://127.0.0.1:9000"
 	return &cbauthimpl.Cache{
@@ -66,12 +55,16 @@ func newCache(a *authImpl) *cbauthimpl.Cache {
 	}
 }
 
+type testingUser struct {
+	user     string
+	domain   string
+	password string
+}
+
 type testingRoundTripper struct {
 	t       *testing.T
 	baseURL string
-	user    string
-	domain  string
-	token   string
+	users   []testingUser
 	tripped bool
 }
 
@@ -137,45 +130,58 @@ func (rt *testingRoundTripper) permissionsRoundTrip(req *http.Request) (res *htt
 
 	statusCode := 401
 
-	switch domain[0] {
-	case "admin":
+	// for simplicity let's grant only permission that matches the username
+	if permission[0] == user[0] {
 		statusCode = 200
-	case "bucket":
-		if permission[0] == "cluster.bucket["+user[0]+"].data!write" {
-			statusCode = 200
-		}
-	case "anonymous":
-		if permission[0] == "cluster.bucket[default].data!write" {
-			statusCode = 200
-		}
 	}
+
 	return respond(req, statusCode, ""), nil
 }
 
-func (rt *testingRoundTripper) authRoundTrip(req *http.Request) (res *http.Response, err error) {
+func (rt *testingRoundTripper) resetTripped() {
+	rt.tripped = false
+}
+
+func (rt *testingRoundTripper) authRoundTrip(req *http.Request) (
+	res *http.Response, err error) {
 	if rt.tripped {
 		log.Fatalf("Already tripped")
 	}
 
 	rt.tripped = true
 
-	statusCode := 200
-
+	var foundUser *testingUser
 	if req.Header.Get("ns-server-ui") == "yes" {
 		token, err := req.Cookie("ui-auth-q")
-		if err != nil || rt.token != token.Value {
-			statusCode = 401
+		if err != nil {
+			panic("ui-auth-q cookie is required")
+		}
+		for _, user := range rt.users {
+			if user.password == token.Value {
+				foundUser = &user
+				break
+			}
 		}
 	} else {
-		log.Fatal("Expect to be called only with ns-server-ui=yes")
+		username, password, ok := req.BasicAuth()
+		if !ok {
+			log.Fatal("Need basic auth header")
+		}
+		for _, user := range rt.users {
+			if user.password == password &&
+				user.user == username {
+				foundUser = &user
+				break
+			}
+		}
 	}
 
-	response := ""
-	if statusCode == 200 {
-		response = fmt.Sprintf(`{"user": "%s", "domain": "%s"}`, rt.user, rt.domain)
+	if foundUser != nil {
+		response := fmt.Sprintf(`{"user": "%s", "domain": "%s"}`,
+			foundUser.user, foundUser.domain)
+		return respond(req, 200, response), nil
 	}
-
-	return respond(req, statusCode, response), nil
+	return respond(req, 401, ""), nil
 }
 
 func (rt *testingRoundTripper) assertTripped(t *testing.T, expected bool) {
@@ -184,10 +190,9 @@ func (rt *testingRoundTripper) assertTripped(t *testing.T, expected bool) {
 	}
 }
 
-func (rt *testingRoundTripper) setTokenAuth(user, domain, token string) {
-	rt.token = token
-	rt.domain = domain
-	rt.user = user
+func (rt *testingRoundTripper) addUser(user, domain, password string) {
+	u := testingUser{user: user, domain: domain, password: password}
+	rt.users = append(rt.users, u)
 }
 
 func TestStaleBasic(t *testing.T) {
@@ -272,14 +277,70 @@ func TestServicePwd(t *testing.T) {
 	}
 }
 
-func TestTokenAdmin(t *testing.T) {
-	rt := newTestingRT(t)
-	rt.setTokenAuth("Administrator", "admin", "1234567890")
-
+func prepareAuth(rt *testingRoundTripper) *authImpl {
 	a := newAuth(0)
 	a.setTransport(rt)
 
 	must(a.svc.UpdateDB(newCache(a), nil))
+	return a
+}
+
+func assertEqual(t *testing.T, name, expect, actual string) {
+	if expect != actual {
+		t.Errorf("Expect %v to be %v, Got %v", name, expect, actual)
+	}
+}
+
+func assertCreds(t *testing.T, c Creds, name, domain string) {
+	assertEqual(t, "Name", name, c.Name())
+	assertEqual(t, "Domain", domain, c.Domain())
+	if !acc(c.IsAllowed(name)) {
+		t.Errorf("Expect permission %v to be granted", name)
+	}
+	if acc(c.IsAllowed("something else")) {
+		t.Errorf("Expect permissions other than %v not to be granted",
+			name)
+	}
+}
+
+func basicAuthRequest(a *authImpl, user, password string) (Creds, error) {
+	req, err := http.NewRequest("GET", "http://q:11234/_whatever", nil)
+	must(err)
+	req.SetBasicAuth(user, password)
+
+	return a.AuthWebCreds(req)
+}
+
+func TestBasicAuth(t *testing.T) {
+	rt := newTestingRT(t)
+	rt.addUser("user1", "local", "asdasd")
+
+	a := prepareAuth(rt)
+
+	c, err := basicAuthRequest(a, "user1", "asdasd")
+	must(err)
+	rt.assertTripped(t, true)
+	assertCreds(t, c, "user1", "local")
+	rt.resetTripped()
+
+	c, err = basicAuthRequest(a, "user1", "asdasd")
+	must(err)
+	rt.assertTripped(t, false)
+	assertCreds(t, c, "user1", "local")
+
+	c, err = basicAuthRequest(a, "user1", "wrong")
+	rt.assertTripped(t, true)
+	if err == nil {
+		t.Errorf("Should not be authenticated. Creds = %v", c)
+	}
+	assertEqual(t, "error", "Authentication failure", err.Error())
+}
+
+func TestTokenAdmin(t *testing.T) {
+	rt := newTestingRT(t)
+	rt.addUser("Administrator", "admin", "1234567890")
+
+	a := prepareAuth(rt)
 
 	req, err := http.NewRequest("GET", "http://q:11234/_queryStatsmaybe", nil)
 	must(err)
@@ -289,16 +350,7 @@ func TestTokenAdmin(t *testing.T) {
 	c, err := a.AuthWebCreds(req)
 	must(err)
 	rt.assertTripped(t, true)
-
-	assertAdmins(t, c, true, false)
-
-	if c.Name() != "Administrator" {
-		t.Errorf("Expect name to be Administrator")
-	}
-
-	if c.Domain() != "builtin" {
-		t.Errorf("Expect source to be builtin. Got %s", c.Domain())
-	}
+	assertCreds(t, c, "Administrator", "builtin")
 }
 
 func TestUnknownHostPortErrorFormatting(t *testing.T) {
